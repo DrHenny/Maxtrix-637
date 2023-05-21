5b5c1aaac9ac9b473adf33459fb7416f6307084a
BigG207 <100019151+BigG207@users.noreply.github.com>
2023-05-20 17:53:32 -0700

    deleted files

--- run.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-title runserver
-java -Xmx815m -cp bin;lib/*; com.rs.Launcher true true false
-pause
--- tool.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Skyotic
-echo starting...
-"C:/Program Files/Java/jre7/bin/java.exe" -Xmx512m -cp bin;lib/*; com.rs.tools.Unban
-pause
